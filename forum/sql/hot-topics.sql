5203e387e805f7a86f4b91a0d177395980d544a8
Alexey Rogachev <arogachev90@gmail.com>
2018-05-11 18:01:36 +0600

    Hot topics - show last_post_author instead of topic_author

--- forum/sql/hot-topics.sql
+++ forum/sql/hot-topics.sql
@@ -2,11 +2,11 @@ SELECT * FROM (
   SELECT DISTINCT ON (posts.topic_id)
     topics.name AS topic_name,
     posts.content AS last_post_content,
-    users.name AS topic_author,
+    users.name AS last_post_author,
     topics.created_at AS topic_created_at
   FROM forum_posts AS posts
   LEFT JOIN forum_topics AS topics ON posts.topic_id = topics.id
-  LEFT JOIN forum_users AS users ON topics.author_id = users.id
+  LEFT JOIN forum_users AS users ON posts.author_id = users.id
   WHERE posts.topic_id IN (
     SELECT subposts.topic_id
     FROM forum_posts AS subposts
