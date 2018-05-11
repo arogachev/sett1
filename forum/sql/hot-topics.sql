9e80b4e322124bea0b9d45d85a70d5f4c9836010
Alexey Rogachev <arogachev90@gmail.com>
2018-05-11 17:52:08 +0600

    Hot topics must rely on last topics and not last posts

--- forum/sql/hot-topics.sql
+++ forum/sql/hot-topics.sql
@@ -2,17 +2,17 @@ SELECT * FROM (
   SELECT DISTINCT ON (posts.topic_id)
     topics.name AS topic_name,
     posts.content AS last_post_content,
-    users.name AS last_post_author,
-    posts.created_at AS last_post_created_at
+    users.name AS topic_author,
+    topics.created_at AS topic_created_at
   FROM forum_posts AS posts
   LEFT JOIN forum_topics AS topics ON posts.topic_id = topics.id
-  LEFT JOIN forum_users AS users ON posts.author_id = users.id
+  LEFT JOIN forum_users AS users ON topics.author_id = users.id
   WHERE posts.topic_id IN (
     SELECT subposts.topic_id
     FROM forum_posts AS subposts
     GROUP BY subposts.topic_id HAVING COUNT(DISTINCT subposts.author_id) > 4
   )
   ORDER BY posts.topic_id, posts.created_at DESC
-  LIMIT 10
 ) AS t
-ORDER BY t.last_post_created_at DESC
+ORDER BY t.topic_created_at DESC
+LIMIT 10
